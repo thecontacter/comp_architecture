883a692d873934eea7026121e6d02c92c13fb705
Sergii Piatakov <sergii.piatakov@globallogic.com>
2018-11-15 15:45:44 +0200

    adding the new function Mul and changing body of the func Add
    In fact the `Calculator` class doesn't have member-variable, so we can
    mark `Add` and `Mul` as `static` and use them without an instance.
    
    Signed-off-by: Sergii Piatakov <sergii.piatakov@globallogic.com>

--- calculator.cpp
+++ calculator.cpp
@@ -2,10 +2,15 @@
 
 int Calculator::Add (double a, double b)
 {
-    return a + b + 0.5;
+    return a + b + 1.5;
 }
 
 int Calculator::Sub (double a, double b)
 {
     return Add (a, -b);
+}
+
+int Calculator::Mul(double a, double b)
+{
+    return (a + 1.5) * (b - 3);
 }
